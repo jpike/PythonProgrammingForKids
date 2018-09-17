762e292d51438c85754b9932c3593f5f08dcb607
jpike <jacob@jacobpike.com>
2018-09-17 19:16:57 -0400

    Adding chapter 2 syntax errors.

---
+++ BasicConcepts/SyntaxErrors/Volume1_Chapter2_SyntaxErrors.py
@@ -0,0 +1,14 @@
+# This program contains various "syntax" errors.
+# It's your job to fix this program so that it runs correctly.
+
+PRINT('Hello, world!')
+primt('Hello, world!')
+prin('Hello, world!')
+print 'Hello, world!')
+print['Hello, world!')
+print('Hello, world!'
+print(Hello, world!)
+print('Hello, world!)
+print 'Hello, world!'
+print('Hello, world!")
+print(''Hello, world!')
