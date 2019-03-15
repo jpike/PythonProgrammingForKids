b01b48a414d30bd8f57e6354884d3df69fb65660
jpike <jacob@jacobpike.com>
2019-03-14 20:58:39 -0400

    Adding Pygame template

---
+++ Pygame/PygameTemplate.py
@@ -0,0 +1,29 @@
+import pygame
+import sys
+
+# INITIALIZE PYGAME.
+pygame.init()
+
+# CREATE THE GAME SCREEN.
+SCREEN_WIDTH_IN_PIXELS = 400
+SCREEN_HEIGHT_IN_PIXELS = 400
+screen = pygame.display.set_mode(
+    (SCREEN_WIDTH_IN_PIXELS, SCREEN_HEIGHT_IN_PIXELS))
+
+# RUN THE GAME UNTIL THE USER CHOOSES TO EXIT.
+while True:
+    # HANDLE ANY EVENTS SENT TO THE GAME PROGRAM.
+    for event in pygame.event.get():
+        # EXIT THE GAME IF THE USER CHOSE TO QUIT.
+        if pygame.QUIT == event.type:
+            # pygame.quit() may still leave the window up,
+            # so sys.exit() is needed to fully close the program.
+            pygame.quit()
+            sys.exit()
+
+    # CLEAR THE SCREEN FOR THE NEW FRAME.
+    BACKGROUND_COLOR = pygame.Color(0, 0, 0)
+    screen.fill(BACKGROUND_COLOR)
+
+    # DISPLAY THE UPDATED FRAME.
+    pygame.display.update()
